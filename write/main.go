f87e270504fb76a5dc8815a2dc77f34fc7bfe8ba
snowtoslow <snowtoslow@mail.ru>
2020-07-28 23:03:53 +0300

    git ignore is now added

--- write/main.go
+++ write/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
 func main() {
-	$END$
+
+	listen , err := net.Listen("tcp",":8084")
+	if err!=nil {
+		log.Panic(err)
+	}
+
+	defer listen.Close()
+
+	for  {
+		connection, err := listen.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(connection, "\nHello from TCP serve\n")
+		fmt.Fprintln(connection, "HOW IS YOUR DAY?")
+		fmt.Fprintf(connection, "%v","Well,I hope!")
+
+		connection.Close()
+	}
+
+
+	
 }
