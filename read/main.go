2109c111e439f68c33b431f4a71586f5c6b55853
snowtoslow <snowtoslow@mail.ru>
2020-07-30 22:29:33 +0300

    add a new directory read and change a little way of my server is working

---
+++ read/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+"bufio"
+"fmt"
+"log"
+"net"
+)
+
+func main() {
+
+	listen , err := net.Listen("tcp",":8084")
+	if err!=nil {
+		log.Panic(err)
+	}
+
+	defer listen.Close()
+
+	for  {
+		connection, err := listen.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(connection)
+	}
+
+
+
+}
+
+func handle(conn net.Conn){
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+	defer conn.Close()
+}
+
