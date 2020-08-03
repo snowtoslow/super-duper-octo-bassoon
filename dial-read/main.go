826c43711a2996417df30b5aa1494307b60d4a02
snowtoslow <snowtoslow@mail.ru>
2020-08-03 22:57:30 +0300

    something

--- dial-read/main.go
+++ dial-read/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+)
+
 func main() {
-	
+
+	connection, err := net.Dial("tcp", "localhost:8084")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer connection.Close()
+
+	bs, err := ioutil.ReadAll(connection)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(bs))
+
 }
--- dial-write/main.go
+++ dial-write/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
-	
+
+	connection, err := net.Dial("tcp", "localhost:8084")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer connection.Close()
+
+	fmt.Fprintln(connection, "I dialed you!")
+
 }
